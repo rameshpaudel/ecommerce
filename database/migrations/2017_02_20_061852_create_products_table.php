1b1d42d0eedfa3f58314669cc8e09a4809bd10b0
Ramesh Paudel <rpaudel58@gmail.com>
2017-02-20 13:58:46 +0545

    Added media and product

---
+++ database/migrations/2017_02_20_061852_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('brand');
+            $table->integer('price');
+            $table->boolean('in_stock');
+            $table->integer('quantity');
+            $table->text('description');
+            $table->text('short_description');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,7 @@
  */
 
 require('./bootstrap');
+require('./../talvbansal/media-manager/js/media-manager');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
